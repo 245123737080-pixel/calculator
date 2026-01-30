5e593d8f47c5e4e06b7a0ee5fdb4b7409e05193a
245123737080-pixel <245123737080@mvsrec.edu.in>
2026-01-30 11:22:07 +0530

    Implement basic calculator operations in Java
    Fix errors in calculator class and improve output.

---
+++ calculator.java
@@ -0,0 +1,15 @@
+class calculator
+  {
+    public static void main(string args[])
+    {
+      int n1=Integer.parseInt(args[0]);
+      int n2=Integer.parseInt(args[1]);
+      int r1=n1+n2;
+      int r2=n1-n2;
+      int r3=n1*n2;
+      int r4=n1/n2;
+      system.out.println("addition=",r1);
+      system.out.println("subraction=",r2);
+      system.out.println("multiplication=",r3)
+      system.out.println("division=",r4)
+        }}
